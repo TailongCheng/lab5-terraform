461d16d130d552ff7d83ae2ab4f323110405864f
Tailong Cheng <tailong.cheng@dcmail.ca>
2024-04-03 15:26:13 -0400

    Update to main

--- aws.tf
+++ aws.tf
@@ -58,9 +58,9 @@ resource "aws_route_table_association" "aws_pub_sub_assoc" {
 }
 
 resource "aws_security_group" "aws_sg" {
-  Name
-  description
-  vpc_id
+  Name              = "BastionHostSG"
+  description       = "Allow SSH"
+  vpc_id            = aws_vpc.cheng_vpc.id
 
   ingress {
     from_port       = 22
@@ -73,6 +73,9 @@ resource "aws_security_group" "aws_sg" {
 resource "aws_instance" "app_server" {
   ami           = "ami-051f8a213df8bc089"
   instance_type = "t2.micro"
+  subnet_id     = aws_subnet.aws_public_subnets.ids[0]
+
+  security_groups = aws_security_group.aws_sg.id
 
   tags = {
     Name = "ExampleAppServerInstance"
