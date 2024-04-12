6e57cdf92fdd482c83d6961b6d3de457f0ba84a3
Tailong Cheng <tailong.cheng@dcmail.ca>
2024-04-12 11:53:13 -0400

    updates

--- aws.tf
+++ aws.tf
@@ -81,81 +81,111 @@ resource "aws_subnet" "aws_private_subnet_2" {
   }
 }
 
-resource "aws_route_table" "aws_pri_rt_1" {
-  vpc_id            = aws_vpc.aws_my_vpc.id
+# resource "aws_route_table" "aws_pri_rt_1" {
+#   vpc_id            = aws_vpc.aws_my_vpc.id
 
-  route {
-    cidr_block      = "0.0.0.0/0"
-    gateway_id      = aws_nat_gateway.aws_nat_gw_1.id
-  }
+#   route {
+#     cidr_block      = "0.0.0.0/0"
+#     gateway_id      = aws_nat_gateway.aws_nat_gw_1.id
+#   }
+
+#   tags = {
+#     Name = "${var.name}_pri_rt_1"
+#   }
+# }
+
+# resource "aws_route_table" "aws_pri_rt_2" {
+#   vpc_id            = aws_vpc.aws_my_vpc.id
+
+#   route {
+#     cidr_block      = "0.0.0.0/0"
+#     gateway_id      = aws_nat_gateway.aws_nat_gw_2.id
+#   }
+
+#   tags = {
+#     Name = "${var.name}_pri_rt_2"
+#   }
+# }
+
+# # NAT Gateway # Can be comment out
+# resource "aws_eip" "aws_nat_eip_1" {
+#   domain = "vpc"
+
+#   tags = {
+#     Name = "${var.name}_nat_eip_1"
+#   }
+# }
+
+# resource "aws_eip" "aws_nat_eip_2" {
+#   domain = "vpc"
+
+#   tags = {
+#     Name = "${var.name}_nat_eip_2"
+#   }
+# }
+
+# resource "aws_nat_gateway" "aws_nat_gw_1" {
+#   allocation_id = aws_eip.aws_nat_eip_1.id
+#   subnet_id     = aws_subnet.aws_public_subnet_1.id
+#   depends_on    = [aws_internet_gateway.aws_ig]
+
+#   tags = {
+#     Name = "${var.name}_nat_gw_1"
+#   }
+# }
+
+# resource "aws_nat_gateway" "aws_nat_gw_2" {
+#   allocation_id = aws_eip.aws_nat_eip_2.id
+#   subnet_id     = aws_subnet.aws_public_subnet_2.id
+#   depends_on    = [aws_internet_gateway.aws_ig]
+
+#   tags = {
+#     Name = "${var.name}_nat_gw_2"
+#   }
+# }
+
+# resource "aws_route_table_association" "aws_pri_sub_assoc_1" {
+#   subnet_id         = aws_subnet.aws_private_subnet_1.id
+#   route_table_id    = aws_route_table.aws_pri_rt_1.id
+# }
+
+# resource "aws_route_table_association" "aws_pri_sub_assoc_2" {
+#   subnet_id         = aws_subnet.aws_private_subnet_2.id
+#   route_table_id    = aws_route_table.aws_pri_rt_2.id
+# }
+
+resource "aws_lb" "aws_alb" {
+  name              = "alb"
+  internal          = false
+  load_balancer_type  = "application"
+  security_groups   = [aws_security_group.aws_lb_sg.id]
+  subnets           = [for subnet in aws_subnet.public : subnet.id]
 
   tags = {
-    Name = "${var.name}_pri_rt_1"
+    Environment = "production"
   }
 }
 
-resource "aws_route_table" "aws_pri_rt_2" {
-  vpc_id            = aws_vpc.aws_my_vpc.id
-
-  route {
-    cidr_block      = "0.0.0.0/0"
-    gateway_id      = aws_nat_gateway.aws_nat_gw_2.id
-  }
-
-  tags = {
-    Name = "${var.name}_pri_rt_2"
-  }
+resource "aws_lb_target_group" "aws_app_tg" {
+  name              = "app_target_group"
+  port              = 80
+  protocol          = "HTTP"
+  vpc_id            = "${aws_vpc.main.id}"
 }
 
-# NAT Gateway # Can be comment out
-resource "aws_eip" "aws_nat_eip_1" {
-  domain = "vpc"
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = "${aws_lb.aws_app_tg.arn}"
+  port              = "80"
+  protocol          = "HTTP"
 
-  tags = {
-    Name = "${var.name}_nat_eip_1"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.aws_app_tg.arn}"
   }
-}
-
-resource "aws_eip" "aws_nat_eip_2" {
-  domain = "vpc"
-
-  tags = {
-    Name = "${var.name}_nat_eip_2"
-  }
-}
-
-resource "aws_nat_gateway" "aws_nat_gw_1" {
-  allocation_id = aws_eip.aws_nat_eip_1.id
-  subnet_id     = aws_subnet.aws_public_subnet_1.id
-  depends_on    = [aws_internet_gateway.aws_ig]
-
-  tags = {
-    Name = "${var.name}_nat_gw_1"
-  }
-}
-
-resource "aws_nat_gateway" "aws_nat_gw_2" {
-  allocation_id = aws_eip.aws_nat_eip_2.id
-  subnet_id     = aws_subnet.aws_public_subnet_2.id
-  depends_on    = [aws_internet_gateway.aws_ig]
-
-  tags = {
-    Name = "${var.name}_nat_gw_2"
-  }
-}
-
-resource "aws_route_table_association" "aws_pri_sub_assoc_1" {
-  subnet_id         = aws_subnet.aws_private_subnet_1.id
-  route_table_id    = aws_route_table.aws_pri_rt_1.id
-}
-
-resource "aws_route_table_association" "aws_pri_sub_assoc_2" {
-  subnet_id         = aws_subnet.aws_private_subnet_2.id
-  route_table_id    = aws_route_table.aws_pri_rt_2.id
 }
 
 # Security Group
-resource "aws_security_group" "aws_sg" {
+resource "aws_security_group" "aws_bastion_sg" {
   name              = "BastionHostSG"
   description       = "Allow SSH"
   vpc_id            = aws_vpc.aws_my_vpc.id
@@ -168,14 +198,31 @@ resource "aws_security_group" "aws_sg" {
   }
 }
 
+resource "aws_security_group" "aws_lb_sg" {
+  name              = "LoadBalancerSG"
+  description       = "Allow HTTP Access"
+  vpc_id            = aws_vpc.aws_my_vpc.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+# 
 resource "aws_instance" "app_server" {
   ami           = "ami-051f8a213df8bc089"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.aws_public_subnet_1.id
 
-  security_groups = [aws_security_group.aws_sg.id]
+  security_groups = [
+    aws_security_group.aws_bastion_sg.id,
+    aws_security_group.aws_lb_sg.id
+  ]
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "AppServerInstance"
   }
 }
