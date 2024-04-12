f39ea2712ac54214d4130275af0eea1d1a3d5c07
Tailong Cheng <tailong.cheng@dcmail.ca>
2024-04-12 12:00:54 -0400

    updates

--- aws.tf
+++ aws.tf
@@ -170,7 +170,7 @@ resource "aws_lb" "aws_alb" {
 }
 
 resource "aws_lb_target_group" "aws_app_tg" {
-  name              = "app_target_group"
+  name              = "AppTargetGroup"
   port              = 80
   protocol          = "HTTP"
   vpc_id            = "${aws_vpc.aws_my_vpc.id}"
